fd26ec87cff11c759180934e53d378acb2689116
Roman <kefirr09@gmail.com>
2017-10-20 16:27:01 +0300

    Modesl and views

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Page;
+use App\People;
+use App\Portfolio;
+use App\Service;
+
 class IndexController extends Controller
 {
     public function execute(Request $request)
     {
-        return view('layouts.site');
+        $pages = Page::all();
+        $portfolio = Portfolio::get(['name', 'filter', 'images']);
+        $people = People::take(3)->get();
+        $services = Service::where('id', '<', 20)->get();
+
+        return view('site.index');
     }
 }
